3b59c5f1461745fa71987a7c110508cb63b71488
Amanda Nobre <34220031+AndersonSilvaCavalcante@users.noreply.github.com>
2024-08-11 15:31:36 -0300

    :sparkles: feat: criar produtos

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,102 +1,186 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import {
+  Image,
+  StyleSheet,
+  Modal,
+  Pressable,
+  View,
+  TextInput
+} from 'react-native';
+
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from 'react';
+import { Dropdown } from 'react-native-element-dropdown';
+import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
+
+import React from 'react';
+type TStatus = 'Em andamento' | 'Aguardando entregador' | 'Em entrega' | 'Concluído'
+
+interface IProducts {
+  id: number,
+  name: string,
+  price: number
+}
 
 export default function TabTwoScreen() {
+
+  const defaultRequest: IProducts = {
+    id: 1,
+    name: '',
+    price: 0,
+  }
+
+  const [listProducts, setlistProducts] = useState<IProducts[]>([])
+  const [itensSelected, setItensSelected] = useState<IProducts[]>([])
+  const [createVisible, setCreateVisible] = useState<boolean>(false)
+
+  // form 
+  const [request, setRequests] = useState<IProducts>(defaultRequest)
+
+  const addListRequest = () => {
+    const exist = listProducts.filter(f => f.id == request.id)[0]
+
+    if (exist) {
+      setlistProducts(listProducts.filter(f => f.id == request.id ? { ...request, itens: itensSelected } : request))
+    } else {
+      setlistProducts(prev => ([...prev, { ...request, itens: itensSelected }]))
+    }
+
+    setRequests({ ...defaultRequest, id: request.id + 1 })
+    setCreateVisible(!createVisible)
+    setItensSelected([])
+    localStorage.setItem('products', JSON.stringify(listProducts))
+  }
+
+  const onChange = (value: string, name: string) => {
+    setRequests({ ...request, [name]: value })
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+          <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+          />
+        }>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Produtos</ThemedText>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setCreateVisible(!createVisible)}>
+            <ThemedText>Adicionar</ThemedText>
+          </Pressable>
+        </ThemedView>
+        <ThemedView>
+
+        </ThemedView>
+        {listProducts.map((request, index) => (
+          <ThemedView style={styles.stepContainer} key={index}>
+            <ThemedText type="subtitle">{request.id}</ThemedText>
+            <ThemedText type="subtitle">Nome: {request.name}</ThemedText>
+            <ThemedText type="subtitle">Valor: R${request.price}</ThemedText>
+          </ThemedView>
+        ))}
+
+      </ParallaxScrollView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={createVisible}
+        onRequestClose={() => {
+          setCreateVisible(!createVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <ThemedText type="subtitle">Adicionar Produto</ThemedText>
+            <ThemedText>Nome</ThemedText>
+            <TextInput
+              style={styles.input}
+              onChangeText={(value: string) => onChange(value, 'name')}
+              value={request.name}
+            />
+            <ThemedText>Valor</ThemedText>
+            <TextInput
+              style={styles.input}
+              onChangeText={(value: string) => onChange(value, 'price')}
+              value={request.price as unknown as string}
+              keyboardType="numeric"
+            />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => addListRequest()}>
+              <ThemedText>Adicionar</ThemedText>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setCreateVisible(!createVisible)}>
+              <ThemedText>Cancelar</ThemedText>
+            </Pressable>
+
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  input: {
+    height: 30,
+    borderWidth: 1
   },
   titleContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
     gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
   },
 });
